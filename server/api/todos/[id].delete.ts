d13614bb27511fd2e8cf2e6083f8538a40ac4574
Sergeyu <sergeyu@kmslh.com>
2024-06-26 19:05:48 +0300

    refactor: fix CRUD

--- server/api/todos/[id].delete.ts
+++ server/api/todos/[id].delete.ts
@@ -2,8 +2,6 @@ import Todo from '@/server/models/Todo';
 import { Types } from 'mongoose';
 export default defineEventHandler(async (event) => {
     const id = event.context.params?.id
-    // const articleId = new Types.ObjectId(event.context.params?.id);
-    // console.log(articleId);
 
     Todo.findOneAndDelete({id: id})
         .then((doc) => {
--- server/api/todos/[id].put.ts
+++ server/api/todos/[id].put.ts
@@ -3,7 +3,6 @@ import Todo from '@/server/models/Todo';
 export default defineEventHandler(async (event) => {
     const id = event.context.params?.id
     const { isDone } = await readBody(event);
-    console.log('put')
     await Todo.findOneAndUpdate({id: id}, { isDone: isDone }, { new: true })
         .then((doc) => {
             console.log(doc)
